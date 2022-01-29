b7fd60b4879af408e3802436711a0856114aebfa
Trishy25 <trish.madzimbamuto@gmail.com>
2022-01-29 20:25:18 +0100

    hmwrk1

---
+++ javascript/javascript1/week1/house estimator.js
@@ -0,0 +1,17 @@
+const Peter = 8*10*10 * 2.5 * 1000 + 100 * 300;
+const Julia = 5*11*8 * 2.5 * 1000 + 70 * 300;
+
+const peterHouseCost = 2500000;
+const juliaHouseCost = 1000000;
+
+if(Peter>peterHouseCost){
+    console.log("Peter is Paid too little")
+} else{
+    console.log("Peter is Paid too much")
+}
+
+if(Julia>juliaHouseCost){
+    console.log("Julia is Paid too little")
+} else{
+    console.log("Julia is Paid too much")
+}
